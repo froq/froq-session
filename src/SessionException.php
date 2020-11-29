645e0b9c3d7756154bf635c56c1fba6873cb6818
Kerem Güneş <qeremy@gmail.com>
2020-11-29 08:06:57 +0300

    Drop finals from exceptions.

--- src/SessionException.php
+++ src/SessionException.php
@@ -17,5 +17,5 @@ use froq\common\Exception;
  * @author  Kerem Güneş <k-gun@mail.com>
  * @since   1.0
  */
-final class SessionException extends Exception
+class SessionException extends Exception
 {}
