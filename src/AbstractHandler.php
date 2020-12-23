726998b95c5e7ecd33969b802ce94915f3b15768
Kerem Güneş <qeremy@gmail.com>
2020-12-23 07:10:54 +0300

    Head doc-ups, doc-ups.

--- src/AbstractHandler.php
+++ src/AbstractHandler.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Copyright (c) 2015 · Kerem Güneş
- * Apache License 2.0 <https://opensource.org/licenses/apache-2.0>
+ * Apache License 2.0 · http://github.com/froq/froq-session
  */
 declare(strict_types=1);
 
@@ -15,7 +15,7 @@ use SessionHandlerInterface;
  *
  * @package froq\session
  * @object  froq\session\AbstractHandler
- * @author  Kerem Güneş <k-gun@mail.com>
+ * @author  Kerem Güneş
  * @since   1.0
  */
 abstract class AbstractHandler implements SessionHandlerInterface
--- src/SessionException.php
+++ src/SessionException.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Copyright (c) 2015 · Kerem Güneş
- * Apache License 2.0 <https://opensource.org/licenses/apache-2.0>
+ * Apache License 2.0 · http://github.com/froq/froq-session
  */
 declare(strict_types=1);
 
@@ -14,7 +14,7 @@ use froq\common\Exception;
  *
  * @package froq\session
  * @object  froq\session\SessionException
- * @author  Kerem Güneş <k-gun@mail.com>
+ * @author  Kerem Güneş
  * @since   1.0
  */
 class SessionException extends Exception
