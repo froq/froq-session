6efbc2f23d13bd2501f9863628c04ee535bef60d
Kerem Güneş <qeremy@gmail.com>
2022-03-06 11:16:24 +0300

    Nope..

--- src/Session.php
+++ src/Session.php
@@ -83,7 +83,7 @@ final class Session implements Arrayable, Objectable, \ArrayAccess
                 throw new SessionException('Given path is a file/link [path: %s]', $path);
             } elseif ($path->isDirectory() && !$path->isAvailable()) {
                 throw new SessionException('Given path is not readable/writable [path: %s]', $path);
-            } elseif (!$path->isDirectory() && !$path->makeDirectory(0755, true)) {
+            } elseif (!$path->isDirectory() && !$path->makeDirectory()) {
                 throw new SessionException('Cannot make directory `savePath` option [path: %s, error: %s]',
                     [$path, '@error']);
             }
